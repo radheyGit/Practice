c2da790724aa4055b41e50b0e2a6ea8d321561b3
radheyGit <radheygarode07@gmail.com>
2022-06-30 07:34:45 +0530

    [Radhey]MyAddition

---
+++ src/com/demo/addition/MyAddition.java
@@ -0,0 +1,56 @@
+package com.demo.addition;
+
+public class MyAddition {
+	
+	int num1,num2,sum;
+	
+	public MyAddition() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public MyAddition(int num1, int num2, int sum) {
+		super();
+		this.num1 = num1;
+		this.num2 = num2;
+		this.sum = sum;
+	}
+
+	public void addition1() {
+
+	}
+	
+	public void addition2(int num1, int num2) {
+
+	}
+	
+	public int addition3(int num1, int num2) {
+		return 0;
+	}
+	
+	public int addition4() {
+		return 0;
+	}
+	
+	public void addition11() {
+
+	}
+	
+	public static void addition22(int num1, int num2) {
+
+	}
+	
+	public static int addition33(int num1, int num2) {
+		return 0;
+	}
+	
+	public static int addition44() {
+		return 0;
+	}
+	
+	
+	
+
+	
+	
+}
---
+++ src/com/demo/addition/Test.java
@@ -0,0 +1,12 @@
+package com.demo.addition;
+
+public class Test {
+
+	/***
+	 * create object of MyAddition class and call all 
+	 * methods here after performing addition in each method
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
